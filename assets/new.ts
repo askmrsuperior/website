d446adf4c33a3e2fed0a365470ecece68e484f00
Suthar Meet <134724945+askmrsuperior@users.noreply.github.com>
2024-03-30 21:53:18 -0400

    hi
    hi

---
+++ assets/new.ts
@@ -0,0 +1,10 @@
+import { Sutharme } from '../app/sutharme';
+
+export const userInfo: Sutharme = {
+  name: "Meet Suthar",
+  studentNumber: 991714979,
+  login: "sutharme",
+  sheridanEmail: "sutharme@sheridancollege.ca",
+  campus: "Trafalgar",
+  assignmentTitle: "A3"
+};
